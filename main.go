bb3dd42f0aa22293146b1ffaeba4c8e3922e491f
Akifumi Fukuda <a.fukuda@mtl.t.u-tokyo.ac.jp>
2020-05-29 05:56:04 +0900

    適当に実装

--- main.go
+++ main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"os"
+	"strings"
+)
 
 func main() {
-	fmt.Println("hello")
+	n := flag.Int("n", 1, "the number of columns")
+	d := flag.String("d", "\t ", "separator of input")
+	sep := flag.String("s", "\t", "separator of output")
+	flag.Parse()
+
+	s := bufio.NewScanner(os.Stdin)
+
+	buf := make([][]string, 0, 32)
+	colnum := 0
+	for s.Scan() {
+		f := strings.FieldsFunc(s.Text(), func(r rune) bool { return strings.ContainsRune(*d, r) })
+		buf = append(buf, f)
+
+		if colnum < len(f) {
+			colnum = len(f)
+		}
+	}
+
+	for i := 0; i < colnum; i += *n {
+		for j := 0; j < len(buf); j++ {
+			l := strings.Join(buf[j][i:i+*n], *sep)
+			fmt.Println(l)
+		}
+	}
 }
